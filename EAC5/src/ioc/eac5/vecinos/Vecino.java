1b14cf0fdbdab2825bd59b7e3732f4c9e513831c
martinod7 <enzonmd@gmail.com>
2014-11-18 18:58:15 +0100

    Merge origin/master
    Conflicts:
    	EAC5/src/ioc/eac5/Principal.java
    	EAC5/src/ioc/eac5/derramas/GestorDeDerramas.java
    	EAC5/src/ioc/eac5/interfaz/Menu.java
    	EAC5/src/ioc/eac5/vecinos/GestorDeVecinos.java
    	EAC5/src/ioc/eac5/vecinos/Vecino.java

--- EAC5/src/ioc/eac5/vecinos/Vecino.java
+++ EAC5/src/ioc/eac5/vecinos/Vecino.java
@@ -7,84 +7,91 @@ package ioc.eac5.vecinos;
 
 /**
  * Clase que gestiona todos y cada uno de los datos relacionados con cada vecino
+ *
  * @author Lorenzo
  */
 public class Vecino {
-    
+
     private String identificador;
     private String nombrePiso;
     private String nombrePropietario;
-<<<<<<< HEAD
-    private int telefono;
+    private String telefono;
     private char presencia; //N -> ausente, S -> presente
     private double coeficiente;
     private int tipoC; //0 -> exento, 1 -> no exento
     private char cargo; //Cargo que ocupa en la junta
-    
+    private double totalAPagar;
+
     //Métodos para acceder y modificar los datos de Vecino
-    public String getIdentificador(){
+    public String getIdentificador() {
         return identificador;
     }
-    
-    public void setIdentificador(String identificador){
-        this.identificador=identificador;
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
     }
-    
-    public String getNombrePiso(){
+
+    public String getNombrePiso() {
         return nombrePiso;
     }
-    
-    public void setNombrePiso(String nombrePiso){
-        this.nombrePiso=nombrePiso;
+
+    public void setNombrePiso(String nombrePiso) {
+        this.nombrePiso = nombrePiso;
     }
-    
-    public String getNombrePropietario(){
+
+    public String getNombrePropietario() {
         return nombrePropietario;
     }
-    
-    public void setNombrePropietario(String nombrePropietario){
-        this.nombrePropietario=nombrePropietario;
+
+    public void setNombrePropietario(String nombrePropietario) {
+        this.nombrePropietario = nombrePropietario;
     }
-    
-    public int getTelefono(){
+
+    public String getTelefono() {
         return telefono;
     }
-    
-    public void setTelefono(int telefono){
 
-        this.telefono=telefono;
+    public void setTelefono(String telefono) {
+
+        this.telefono = telefono;
     }
-    
-    public char getPresencia(){
+
+    public char getPresencia() {
         return presencia;
     }
-    
-    public void setPresencia(char presencia){
-        this.presencia=presencia;
+
+    public void setPresencia(char presencia) {
+        this.presencia = presencia;
     }
-    
-    public double getCoeficiente(){
+
+    public double getCoeficiente() {
         return coeficiente;
     }
-    
-    public void setCoeficiente(double coeficiente){
-        this.coeficiente=coeficiente;
+
+    public void setCoeficiente(double coeficiente) {
+        this.coeficiente = coeficiente;
     }
-    
-    public int getTipoC(){
+
+    public int getTipoC() {
         return tipoC;
     }
-    
-    public void setTipoC(int tipoC){
-        this.tipoC=tipoC;
+
+    public void setTipoC(int tipoC) {
+        this.tipoC = tipoC;
     }
-    
-    public char getCargo(){
+
+    public char getCargo() {
         return cargo;
     }
-    
-    public void setCargo(char cargo){
-        this.cargo=cargo;
+
+    public void setCargo(char cargo) {
+        this.cargo = cargo;
     }
-        
+
+    public double getTotalAPagar() {
+        return totalAPagar;
+    }
+    public void setTotalAPagar(){
+        this.totalAPagar = totalAPagar;
+    }
 }
